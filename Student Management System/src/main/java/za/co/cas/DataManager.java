5ea7e36716db6f764f7abac5167aed4d088b9e91
smagwentshu023 <smagwentshu023@wethinkcode.co.za>
2024-07-01 15:49:54 +0200

    Fixed Reading Issue

--- Student Management System/src/main/java/za/co/cas/DataManager.java
+++ Student Management System/src/main/java/za/co/cas/DataManager.java
@@ -1,5 +1,6 @@
 package za.co.cas;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,7 +21,19 @@ public class DataManager extends Mapper {
      * Constructor that sets the file path for storing student data.
      */
     public DataManager() {
-        super(System.getProperty("user.dir")+sp()+"Student Management System"+sp()+"src"+sp() +"main"+sp() +"java"+sp() +"za"+sp() +"co"+sp() +"cas"+sp()+"Students.json");
+        super(System.getProperty("user.dir")+sp()+"Student Management System"+sp()+"src"+sp() +"main"+sp() +"java"+sp() +"za"+sp() +"co"+sp() +"cas"+sp()+ "Students.json");
+        File file = new File(System.getProperty("user.dir")+sp()+"Student Management System"+sp()+"src"+sp() +"main"+sp() +"java"+sp() +"za"+sp() +"co"+sp() +"cas"+sp()+ "Students.json");
+        if (!file.exists()) {
+            try {
+                var i = file.setWritable(true);
+                i = file.createNewFile();
+                Files.writeString(file.toPath(), "[]");
+            } catch (IOException e) {
+                System.out.printf("Cannot create file: %s%n", file.getName());
+            }
+        }
+
+
     }
 
     /**
@@ -30,15 +43,15 @@ public class DataManager extends Mapper {
      */
     public ArrayList<Student> read() {
         ArrayList<Student> studentList = new ArrayList<>();
-        ArrayList<LinkedHashMap<String, ?>> jsonList;
+        List<LinkedHashMap<String, ?>> jsonList;
         try {
             // Read JSON data from the file and parse it into a list of LinkedHashMaps
-            jsonList = mapper.readValue(Files.readString(Path.of(FILE.getAbsolutePath())), mapper.getTypeFactory().constructCollectionType(List.class, ArrayList.class));
+            jsonList = mapper.readValue(Files.readString(Path.of(FILE.getAbsolutePath())), List.class);//mapper.getTypeFactory().constructCollectionType(List.class, ArrayList.class));
 
             // Iterate through the list of student data
             for (HashMap<String, ?> studentHashMap : jsonList) {
                 // Convert each student's data to a HashMap
-                HashMap<String, ?> newStudentMap = mapper.readValue(mapper.writeValueAsString(studentHashMap), mapper.getTypeFactory().constructCollectionType(List.class, HashMap.class));
+                HashMap<String, ?> newStudentMap = mapper.readValue(mapper.writeValueAsString(studentHashMap), HashMap.class);//mapper.getTypeFactory().constructCollectionType(List.class, HashMap.class));
                 Student student = new Student((String) newStudentMap.get("name"));
 
                 // Populate student object with data from the map
